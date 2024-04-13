f650c832f12ea8db73dd014eafbf44f06f3884d0
Magomedcoder <info@magomedcoder.ru>
2024-04-14 01:34:44 +0300

    Feat: added vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,40 @@
-import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import Inspect from 'vite-plugin-inspect'
+import compressPlugin from 'vite-plugin-compression'
+// import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE')
-
-  return {
-    base: env.VITE_BASE,
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      },
-      extensions: ['.vue', '.ts', '.json']
+export default defineConfig({
+  plugins: [
+    vue(),
+    // vueJsx({}),
+    compressPlugin(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass'
+      })]
+    }),
+    // VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    root: process.cwd(),
-    assetsInclude: ['./src/assets'],
-    plugins: [
-      vue(),
-      // vueJsx({}),
-      // compressPlugin(),
-      AutoImport({
-        resolvers: [ElementPlusResolver()]
-      }),
-      Components({
-        resolvers: [ElementPlusResolver({
-          importStyle: 'sass'
-        })]
-      }),
-      // ElementPlus({
-      //   useSource: true,
-      //   defaultLocale: 'ru'
-      // }),
-      Inspect()
-    ],
-    define: {
-      __APP_ENV__: env.APP_ENV
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use "@/assets/scss/element.scss" as *;`
-        }
+    extensions: ['.vue', '.ts', '.json']
+  },
+  assetsInclude: ['./src/assets'],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/scss/element.scss" as *;`
       }
     }
   }
